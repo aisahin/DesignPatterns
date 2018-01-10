09f9a5e3de2ceb9e0ef8c3c4dd6412e654437ed3
Ali İhsan ŞAHİN <aliihsan.sahin@kaleyazilim.com.tr>
2018-01-10 15:28:28 +0300

    First Commit.

---
+++ Creational/src/com/ais/singleton/EagerInitializedSingleton.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018 aliihsan.sahin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ais.singleton;
+
+/**
+ *
+ * @author aliihsan.sahin
+ */
+public class EagerInitializedSingleton {
+    
+    private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+    
+    //private constructor to avoid client applications to use constructor    
+    private EagerInitializedSingleton() {}
+    
+    public static EagerInitializedSingleton getInstance() {
+    
+        return instance;
+    }    
+}
